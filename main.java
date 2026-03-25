d197b35778663ab402c85f11b542c6be36499c06
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-25 23:49:14 +0530

    first push

---
+++ main.java
@@ -0,0 +1,57 @@
+// public class main{
+
+
+//     public static void main(String[] args){
+
+//         // System.out.print("I LIKE PIZZA!");
+//         // System.out.print("its really good");
+
+
+//         System.out.println("I LIKE PIZZA!"); //ln for next line, or System.out.print("I LIKE PIZZA!\n"); also works
+//         System.out.println("its really good");
+//         System.out.print("its yummy!\n");
+//         System.out.print("Buy me pizza!");
+//     }
+    
+
+
+// }
+
+
+public class main{
+
+    public static void main(String[] args){
+        
+
+
+        // INT
+
+         int age=30;
+         int year=2026;
+         int quantity=3;
+         System.out.println("The year is " + year);
+
+        //  DOUBLE
+         
+
+         double price=19;
+         double gpa=6.7;
+         double temperatur=12.5;
+
+         System.out.println("$" + price);
+
+
+
+         
+
+
+
+
+
+
+
+
+    }
+
+
+}
