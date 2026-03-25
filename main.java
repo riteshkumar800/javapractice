a274eb8e60a7fa02ec2fb1f5834837afb05aa1a0
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-26 01:18:13 +0530

    new code added

--- main.java
+++ main.java
@@ -18,40 +18,85 @@
 // }
 
 
-public class main{
+// public class main{
 
-    public static void main(String[] args){
+//     public static void main(String[] args){
         
 
 
-        // INT
+//         // INT
 
-         int age=30;
-         int year=2026;
-         int quantity=3;
-         System.out.println("The year is " + year);
+//          int age=30;
+//          int year=2026;
+//          int quantity=3;
+//          System.out.println("The year is " + year);
 
-        //  DOUBLE
+//         //  DOUBLE
          
 
-         double price=19;
-         double gpa=6.7;
-         double temperatur=12.5;
+//          double price=19;
+//          double gpa=6.7;
+//          double temperatur=12.5;
 
-         System.out.println("$" + price);
-
-
-
-         
+//          System.out.println("$" + price);
 
 
 
 
+//         //  CHAR
+
+//         char grade='A';
+//         char symbol='!';
+//         char currency='$';
+
+//         System.out.println(symbol);
 
 
 
 
-    }
+//         // BOOLEAN
+//         boolean isstudent=true;
+//         boolean forSale=false;
+//         boolean isOnline=true;
+
+//         System.out.println(forSale);
 
 
-}
+//         if(isstudent){
+//             System.out.println("you are a student");
+//         }
+//         else{
+//             System.out.println("You are not a student");
+//         }
+
+
+//         String name="Riitesh";
+//         String food="pizza";
+//         String car="Mustang";
+//         String color="red";
+
+//         System.out.println("Hello "+ name);
+//         System.out.println("you are  "+ age + "years old");
+//         System.out.println("Your gpa is "+ gpa);
+
+
+
+//         System.out.println("your choice is a "+ color+" "+year+ " "+car+" ");
+        
+
+//         if(forSale){
+//             System.out.println("There is "+ car+" for sale");
+
+//         }
+//         else{
+//             System.out.println("The "+ car +" is out of sale");
+//         }
+
+
+//         //
+
+//     }
+// }
+
+// #############################################################################################################################
+
