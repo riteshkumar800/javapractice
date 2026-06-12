dd33347dc051cd2e4bd7c54045597bd50882f329
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-06-12 23:51:34 +0530

    added

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+
+    String name;
+    double gpa;
+    int age;
+    boolean isEnrolled;
+
+    Student(String name, int age, double gpa){
+        this.name=name;
+        this.age=age;
+        this.gpa=gpa;
+        isEnrolled=true;
+
+    }
+
+    
+}
