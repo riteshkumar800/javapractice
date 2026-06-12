fa9c91e7a4a7f2bb7c469a7cbe73bbb1510379a5
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-06-12 23:38:59 +0530

    added

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    String make= "Ford";
+    String model="Mustang";
+    int year= 2026;
+    double price=99.99;
+    boolean isRunning=false;
+
+    void start(){
+        System.out.println("Car started");
+    }
+    
+}
---
+++ object.java
@@ -0,0 +1,15 @@
+public class object {
+    public static void main(String[] args){
+
+
+        Car car = new Car();
+        
+        car.isRunning=true;
+        // System.out.println(car.make);
+        // System.out.println(car.isRunning);
+
+        car.start();
+
+    }
+    
+}
