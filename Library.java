e399c53ad4686565b88ca19cd0a05baf475e1038
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-06-15 23:39:59 +0530

    added

--- Library.java
+++ Library.java
@@ -3,6 +3,16 @@ public class Library {
     String name;
     int year;
     Book[] books;
-    
+
+
+    Library(String name, int year, Book[] books ){
+        this.name=name;
+        this.year=year;
+        this.books=books;
+    }
+
+    void displayInfo(){
+        System.out.println("The "+ this.year+  " " + this.name);
+    }
     
 }
