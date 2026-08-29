7d69f1d8e9d93a90caf7d6f7621db5e249220081
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-08-29 21:40:24 +0530

    added

---
+++ Telusko/oops/demo1.java
@@ -0,0 +1,24 @@
+ class calc{
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+}
+
+
+public class demo1{
+
+public static void main(String[] args) {
+
+    calc c=new calc();
+    int r1=c.add(3,4);
+    System.out.println(r1);
+
+
+
+    
+}
+
+}
---
+++ Telusko/oops/demo2.java
@@ -0,0 +1,18 @@
+
+class Advcalc extends calc{
+
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return b==0? 0: a/b;
+    }
+
+
+}
+
+public class demo2{
+public static void main(String[] args) {
+    
+}
+}
