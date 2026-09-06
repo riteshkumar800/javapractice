a6bd5b4891faa5ee84eeaa94618b8ee52a91ad64
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-09-06 19:14:45 +0530

    added

---
+++ Telusko/oops/optional.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class optional {
+
+    public static void main(String[] args) {
+
+        List<String> names=Arrays.asList("Navin","ritesh","raj","ravi");
+ 
+        Optional<String> name = names.stream()
+        .filter(str ->  str.contains("x"))
+        .findFirst();
+    
+    
+    
+    
+
+        
+        
+    }
+    
+}
